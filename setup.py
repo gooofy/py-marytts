f2693531ea841d21a7b94be0304c3dc8f1d9e5f7
Guenter Bartsch <guenter@zamia.org>
2018-03-17 13:10:39 +0100

    LGPL -> Apache-2.0 license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name                 ='py-marytts',
-      version              ='0.1.3',
+      version              ='0.1.4',
       description          ='Python interface for Mary TTS',
       long_description     = open('README.md').read(),
       author               = 'Guenter Bartsch',
@@ -15,14 +15,14 @@ setup(name                 ='py-marytts',
                               'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
                               'Topic :: Multimedia :: Sound/Audio :: Speech',
                               'Operating System :: POSIX :: Linux',
-                              'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                              'License :: OSI Approved :: Apache Software License',
                               'Programming Language :: Python :: 2',
                               'Programming Language :: Python :: 2.7',
                               'Programming Language :: Python :: 3',
                               'Programming Language :: Python :: 3.4',
                              ],
       platforms            = 'Linux',
-      license              = 'LGPLv3',
+      license              = 'Apache',
       package_dir          = {'marytts': 'marytts'},
       packages             = ['marytts']
       )
